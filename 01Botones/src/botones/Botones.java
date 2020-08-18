765c7fb79c7238c4b5bf2462d600d2aa8bf2100d
thelegendkiller <52903659+thelegendkiller@users.noreply.github.com>
2020-08-18 16:41:45 -0500

    Actualización

--- 01Botones/src/botones/Botones.java
+++ 01Botones/src/botones/Botones.java
@@ -9,10 +9,15 @@ import javax.swing.JFrame;
 public class Botones {
 
     public static void main(String[] args) {
+        PanelBotones panel = new PanelBotones();            // Vista
+        OyenteBotones oyente = new OyenteBotones(panel);    // Controlador
+        panel.addEventos(oyente);
         JFrame f = new JFrame("Ventana Botones");
+        
         f.setSize(800, 600);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLocationRelativeTo(f);
+        f.add(panel);
         f.setVisible(true);
     }
     
