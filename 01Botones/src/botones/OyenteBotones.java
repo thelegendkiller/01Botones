639d8c3ff628fb2ac7126970aee69ac889830f7e
thelegendkiller <52903659+thelegendkiller@users.noreply.github.com>
2020-08-18 17:08:07 -0500

    Actualización comentarios

--- 01Botones/src/botones/OyenteBotones.java
+++ 01Botones/src/botones/OyenteBotones.java
@@ -6,19 +6,24 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 /**
- *
- * @author DanielCruz
- */
+ * Esta es la clase OyenteBotones, la cual actuara como oyente.
+ * En este tipo de oyentes, se les puede poner una clase adaptadora, ya que implementan
+   las interfaces y que se pueden utilizar para construir oyentes.*/
 public class OyenteBotones implements ActionListener {
 
-    private final PanelBotones panel;
+    private final PanelBotones panel; // Aquí inicizamos a la clase PanelBotones
     
-    public OyenteBotones(PanelBotones panel) {
+    public OyenteBotones(PanelBotones panel) { // Aquí le presentamos el panel al oyente
         this.panel = panel;
     }
     
+    /**
+     * Aquí tenemos un evento llamado ActionEvent, que este tiene a su interfaz Oyente llamada
+       ActionListener y trae como método asociado al actionPerformed(ActionEvent e)
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Aquí tenemos una iteracción, ya que el panel al tener 3 botones, necesitamos conocer su origen
         JButton origen = (JButton) e.getSource();
         
         switch(origen.getName()) {
