d54888bc98392c64872afc64527c81c0443564ba
thelegendkiller <52903659+thelegendkiller@users.noreply.github.com>
2020-08-18 16:10:39 -0500

    Archivo Botones.java

---
+++ 01Botones/src/botones/Botones.java
@@ -0,0 +1,19 @@
+package botones;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DanielCruz
+ */
+public class Botones {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Ventana Botones");
+        f.setSize(800, 600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(f);
+        f.setVisible(true);
+    }
+    
+}
